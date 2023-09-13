e8f3ab12d10ccfb89813967a38f2087f6feb67df
Marcus Hakozaki <marcus.hakozaki@gmail.com>
2023-09-13 14:56:06 -0500

    Cria funções de search e find

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        return view('articles.index', [
+            'articles' => Article::all(),
+        ]);
+    }
+
+    public function search(Request $request)
+    {
+        $articles = $this->find($request->search);
+
+        return view('articles.index', [
+            'articles' => $articles,
+        ]);
+    }
+
+    private function find(string $query = ''): Collection
+    {
+        return Article::query()
+            ->where('body', 'like', "%{$query}%")
+            ->orWhere('title', 'like', "%{$query}%")
+            ->get();
+    }
+}
