bb5f7fd609221a6827be466762ed0a05ef5241d4
Marcus Hakozaki <marcus.hakozaki@gmail.com>
2023-09-13 21:24:16 -0500

    Ajuste da tela de Article para tailwind

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,33 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="card">
-        <div class="card-header">
-            Articles <small>({{ $articles->count() }})</small>
-        </div>
-        <div class="card-body">
-            <form action="{{ url('search') }}" method="get">
-                <div class="form-group">
-                    <input type="text" name="search" class="form-control" placeholder="Search..." value="{{ request('search') }}"/>
-                </div>
-            </form>
-            @forelse ($articles as $article)
-            <article class="mb-3">
-                <h2>{{ $article->title }}</h2>
-
-                <p class="m-0">{{ $article->body }}</body>
-
-                <div>
-                    @foreach ($article->tags as $tag)
-                    <span class="badge badge-light">{{ $tag}}</span>
-                    @endforeach
-                </div>
-            </article>
-            @empty
-            <p>No articles found</p>
-            @endforelse
-        </div>
+<form action="{{ url('article/search') }}" method="get">
+    <div class="form-group">
+        <input type="text" name="search" class="form-control" placeholder="Search..." value="{{ request('search') }}" />
     </div>
-</div>
+</form>
+<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+            <th scope="col" class="px-6 py-3">
+                Title
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Body
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Tags
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($articles as $article)
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ $article->title }}
+            </th>
+            <td class="px-6 py-4">
+                {{ $article->body }}
+            </td>
+            <td class="px-6 py-4">
+                @foreach ($article->tags as $tag)
+                <span class="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">{{$tag}}</span>
+                @endforeach
+            </td>
+        </tr>
+        @empty
+        <p>No articles found</p>
+        @endforelse
+    </tbody>
+</table>
 @stop
