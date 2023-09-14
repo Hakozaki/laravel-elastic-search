78bb6cd541f3ef32600a04a15d754e75712e729a
Marcus Hakozaki <marcus.hakozaki@gmail.com>
2023-09-13 21:28:38 -0500

    Ajuste do input de search

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <form action="{{ url('article/search') }}" method="get">
     <div class="form-group">
-        <input type="text" name="search" class="form-control" placeholder="Search..." value="{{ request('search') }}" />
+        <input type="text" name="search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." value="{{ request('search') }}" />
     </div>
 </form>
 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
